056c3e53f026bc9210ea6c23242b89aa72db497e
Hugo <hugocosta8c2@gmail.com>
2023-08-20 18:28:14 -0300

    cadastro de usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.urls import reverse
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não coincidem.')
+            return redirect(reverse('cadastro'))
+
+        # TODO vlidar a força da senha
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Usuário já existe.')
+            return redirect('/usuarios/cadastro')
+
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        messages.add_message(request, constants.SUCCESS, 'Usuário salvo com sucesso.')
+
+
+        return redirect(reverse('login'))
